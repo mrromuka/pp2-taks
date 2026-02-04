5ffd3f39e02b5f3ef0dc178072d3ebd0a204e92b
mrromuka <andthemuka@gmail.com>
2026-02-04 12:28:38 +0500

    Add Practice2 - Python control flow examples (boolean, if-else, loops)

---
+++ prac2/if else/If Statement.py
@@ -0,0 +1,9 @@
+#1
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+  #2
+number = 15
+if number > 0:
+  print("The number is positive")
---
+++ prac2/if else/if else.py
@@ -0,0 +1,14 @@
+#1
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+  #2
+number = 7
+
+if number % 2 == 0:
+  print("The number is even")
+else:
+  print("The number is odd")
